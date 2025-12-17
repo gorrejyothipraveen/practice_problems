3329b59a1df91d8a00b7bd875dec7fafd6d00c7c
Jyothi Praveen Gorre <jyothipraveengorre@Jyothis-MacBook-Pro.local>
2025-12-17 10:32:11 +0530

    path normalization is almost ready

--- src/path_normalization.js
+++ src/path_normalization.js
@@ -1,3 +1,23 @@
-export const resolvePath = () => {
-  return '/';
-}
+const CURRENT_DIR = '.';
+const PARENT_DIR = '..';
+
+export const pathNormalizer = (path) => {
+  const instructionsAndCommands = path.split(/\/+/);
+  instructionsAndCommands.shift()
+  const result = instructionsAndCommands.reduce((stack, instruction) => {
+    if (instruction === PARENT_DIR && stack.length !== 0) {
+      stack.pop();
+    }
+    if (instruction !== CURRENT_DIR && instruction !== PARENT_DIR) {
+      stack.push(instruction);
+    }
+    return [...stack];
+  }, []);
+
+  return result.join("/");
+};
+
+export const resolvePath = (path) => {
+  const resolvedPath =  '/' + pathNormalizer(path);
+  return resolvedPath;
+};
--- test/path_normalization_test.js
+++ test/path_normalization_test.js
@@ -1,8 +1,32 @@
 import { assertEquals } from "@std/assert";
-import { resolvePath } from "../src/path_normalization.js";
+import { pathNormalizer, resolvePath } from "../src/path_normalization.js";
 
 Deno.test("simple test", () => {
   const path = '/../'
   assertEquals(resolvePath(path), '/');
 });
-    
+
+Deno.test("/a = /a", () => {
+  const path = '/a'
+  assertEquals(pathNormalizer(path), 'a');
+});
+
+Deno.test("/a/b/c = /a/b/c", () => {
+  const path = '/a/b/c'
+  assertEquals(pathNormalizer(path), 'a/b/c');
+});
+
+Deno.test("/a/../c = /c", () => {
+  const path = '/a/../c'
+  assertEquals(pathNormalizer(path), 'c');
+});
+
+Deno.test("/../../../c = /c", () => {
+  const path = '/../../../c'
+  assertEquals(pathNormalizer(path), 'c');
+});
+
+Deno.test("simple test", () => {
+  const path = '/../../..'
+  assertEquals(resolvePath(path), '/');
+});
